af9990287f771bd8f284e4407a78b722cbc1b46a
Chi Kei Chan <chikeichan@gmail.com>
2018-09-30 20:38:06 -0700

    Add Onboarding Siderbar

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { connect } from "react-redux";
 import './Onboarding.css';
+import { OnboardingSidebar } from 'kyokan-ui';
+import logo from "../../assets/logo.svg";
+import icon from "../../assets/icon_developers.svg";
 
 class Onboarding extends React.Component {
 
@@ -14,9 +17,19 @@ class Onboarding extends React.Component {
 
   render () {
     return (
-      <div>
-        <h1>Onboarding</h1>
-        <pre>{ JSON.stringify(this.props) }</pre>
+      <div className="onboarding">
+        <OnboardingSidebar
+            className="onboarding__sidebar"
+            logoUrl={logo}
+            logoClassName="onboarding__logo"
+            subheaderText="DEVELOPERS"
+            headerText="Create Machine Learning Algorithmn"
+            descriptionText="Upload your algorithms to the marketplace and get paid each time they run on a device."
+            iconUrl={icon}
+            iconClassName="onboarding__icon"
+            currentStepIndex={0}
+            totalSteps={5}
+        />
       </div>
     );
   }
