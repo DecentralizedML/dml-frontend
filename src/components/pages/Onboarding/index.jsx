8afec157363708a54cd1ae39a276a301f41904d7
Chi Kei Chan <chikeichan@gmail.com>
2018-10-01 21:57:46 -0700

    Add AdditionalInfo Screen

---
+++ src/components/pages/Onboarding/index.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { connect } from "react-redux";
+import './Onboarding.css';
+import Signup from './Signup';
+import AdditionalInfo from './AdditionalInfo';
+
+class Onboarding extends React.Component {
+
+  /*
+  shouldComponentUpdate (nextProps) {
+    console.log(this.props);
+    console.log(nextProps);
+    return true;
+  }
+  */
+
+  render () {
+    return <AdditionalInfo />
+  }
+}
+
+export default connect(
+  state => state,
+  dispatch => ({
+  })
+)(Onboarding);
