bf088f1f35c823529ee3d9274d7654cf7675225a
Chi Kei Chan <chikeichan@gmail.com>
2018-10-04 19:37:07 -0700

    Connect onboarding pages (#25)

--- src/pages/Onboarding/index.jsx
+++ src/pages/Onboarding/index.jsx
@@ -2,8 +2,12 @@ import * as React from 'react';
 import { connect } from "react-redux";
 import './Onboarding.css';
 import Signup from './Signup';
-// import AdditionalInfo from './AdditionalInfo';
-// import Metamask from './Metamask';
+import AdditionalInfo from "./AdditionalInfo";
+import Metamask from "./Metamask";
+
+const SIGNUP = 0;
+const ADDITIONAL_INFO = 1;
+const METAMASK = 2;
 
 class Onboarding extends React.Component {
 
@@ -15,8 +19,21 @@ class Onboarding extends React.Component {
   }
   */
 
+  state = {
+    step: SIGNUP,
+  };
+
   render () {
-    return <Signup />
+    switch (this.state.step) {
+      case SIGNUP:
+        return <Signup onComplete={() => this.setState({ step: ADDITIONAL_INFO })}/>;
+      case ADDITIONAL_INFO:
+        return <AdditionalInfo onComplete={() => this.setState({ step: METAMASK })}/>;
+      case METAMASK:
+        return <Metamask />;
+      default:
+        return <noscript />;
+    }
   }
 }
 
