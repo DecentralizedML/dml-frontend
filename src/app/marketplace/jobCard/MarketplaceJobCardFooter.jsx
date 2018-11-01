e4bd38bcae654fe410b88adccfbf4f2693732cb6
Makzent <arnold.michael91@gmail.com>
2018-11-01 13:02:58 +0900

    WIP card footer - added icons to public folder

--- src/app/marketplace/jobCard/MarketplaceJobCardFooter.jsx
+++ src/app/marketplace/jobCard/MarketplaceJobCardFooter.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 
 import { Box, Column, Grid, Header, Row } from "@kyokan/kyokan-ui";
 
-const Downloads = styled.p`
+const NumberValue = styled.p`
   width: 36px;
   height: 21px;
   font-family: Barlow;
@@ -15,17 +15,50 @@ const Downloads = styled.p`
   color: #8194b2;
 `;
 
+const DownloadIcon = styled.img`
+  width: 14px;
+  height: 14px;
+  object-fit: contain;
+  padding-right: 6px;
+`;
+
+const Star = styled.img`
+  width: 14px;
+  height: 14px;
+  object-fit: contain;
+`;
+
 const CardFooter = props => {
-  const { downloads, averageRating, totalRatings, priceValue } = props;
+  const { downloads, averageRating, totalRatings, rewardValue } = props;
 
   const roundedDownloads =
     downloads > 1000 ? `${Math.round(downloads / 100) / 10}k` : downloads;
 
-  console.log(roundedDownloads);
+  const renderStars = averageRating => {
+    const starsArray = [];
+    for (let i = 0; i < 5; i++) {
+      if (averageRating - i < 0.25) {
+        starsArray.push("emptyStarIcon");
+      } else if (averageRating - i < 0.75) {
+        starsArray.push("halfStarIcon");
+      } else {
+        starsArray.push("fullStarIcon");
+      }
+    }
+    return starsArray;
+  };
+
+  // TODO:
+  // Implement columns to seperate the values
+  // Implement DML button with value
   return (
     <Row nogutter align="center">
-      XX
-      <Downloads>{roundedDownloads}</Downloads>
+      <DownloadIcon src="/downloadIcon.svg" />
+      <NumberValue>{roundedDownloads}</NumberValue>
+      {renderStars(averageRating).map((star, index) => {
+        return <Star key={index} src={`/${star}.svg`} />;
+      })}
+      <NumberValue>{`(${totalRatings})`}</NumberValue>
     </Row>
   );
 };
