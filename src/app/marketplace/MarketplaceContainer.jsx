f7a21773a14e81719675a9b7c63748aa32885acd
Makzent <arnold.michael91@gmail.com>
2018-11-05 16:25:10 +0900

    WIP added actions and reducer for marketplace rendering

--- src/app/marketplace/MarketplaceContainer.jsx
+++ src/app/marketplace/MarketplaceContainer.jsx
@@ -2,18 +2,28 @@ import { connect } from "react-redux";
 
 import MarketplaceComponent from "./MarketplaceComponent";
 
-const mapStateToProps = state => {
-  const { email, firstName, id, lastName, walletAddress } = state.account;
+import marketplaceActions from "./duck/actions";
 
+const mapStateToProps = state => {
+  // const { email, firstName, id, lastName, walletAddress } = state.account;
+  const { category, tag } = state.marketplace;
+  return { category, tag };
+};
+
+const mapDispatchToProps = dispatch => {
   return {
-    email,
-    firstName,
-    id,
-    lastName,
-    walletAddress
+    selectCategory: category => {
+      dispatch(marketplaceActions.selectCategory(category));
+    },
+    selectTag: tag => {
+      dispatch(marketplaceActions.selectTag(tag));
+    }
   };
 };
 
-const MarketplaceContainer = connect(mapStateToProps)(MarketplaceComponent);
+const MarketplaceContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MarketplaceComponent);
 
 export default MarketplaceContainer;
--- src/app/marketplace/duck/actions.js
+++ src/app/marketplace/duck/actions.js
@@ -1,1 +1,17 @@
-export default {};
+import types from "./types";
+
+const selectCategory = category => {
+  return {
+    type: types.SELECT_CATEGORY,
+    payload: category
+  };
+};
+
+const selectTag = tag => {
+  return {
+    type: types.SELECT_TAG,
+    payload: tag
+  };
+};
+
+export default { selectCategory, selectTag };
