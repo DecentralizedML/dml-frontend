f2b89d6c382561b3ab7a51120189f042e7c16e53
Makzent <arnold.michael91@gmail.com>
2018-11-05 16:34:26 +0900

    added initial state

--- src/app/marketplace/duck/reducer.js
+++ src/app/marketplace/duck/reducer.js
@@ -5,14 +5,24 @@ import types from "./types";
 
 // import types from './types';
 
-const marketplaceReducer = (state = {}, action) => {
+const initialState = {
+  category = 'all',
+  tag = 'all',
+  singleView = false,
+  filteredView = false,
+}
+
+const marketplaceReducer = (state = initialState, action) => {
   return produce(state, draftState => {
     switch (action.type) {
       case types.SELECT_CATEGORY:
         draftState["category"] = action.payload;
-
       case types.SELECT_TAG:
         draftState["tag"] = action.payload;
+        draftState["filteredView"] = true;
+      case types.SELECT_TAG:
+        draftState["tag"] = action.payload;
+        draftState["filteredView"] = true;
 
       default:
     }
