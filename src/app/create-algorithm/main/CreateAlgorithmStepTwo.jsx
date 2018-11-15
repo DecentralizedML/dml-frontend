d468fe0bd9281511d76ea3e5bd2bc785ecbe4ee9
Makzent <arnold.michael91@gmail.com>
2018-11-15 11:53:20 +0900

    added drag and drop file uploader

--- src/app/create-algorithm/main/CreateAlgorithmStepTwo.jsx
+++ src/app/create-algorithm/main/CreateAlgorithmStepTwo.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 // import PropTypes from 'prop-types';
+import ReactDropzone from "react-dropzone";
+import request from "superagent";
 
 import {
   Main,
   Headline,
+  SubHeadline,
   DescriptionText,
   Divider,
   UploadAlgorithmWrapper,
@@ -14,18 +17,54 @@ import {
 } from "./UI";
 import CreateAlgorithmNavigationButton from "./components/CreateAlgorithmNavigationButton";
 
-const CreateAlgorithmStepTwo = props => {
-  return (
-    <Main>
-      <Headline>Upload machine learning algorithm</Headline>
-      <DescriptionText>
-        We will run your algorithm using the data you pre-processed in the
-        previous step.
-      </DescriptionText>
+class CreateAlgorithmStepTwo extends Component {
+  state = {
+    uploading: false,
+    fileName: null
+  };
+  onDrop = files => {
+    this.setState({ uploading: true, fileName: files[0].name });
+    // POST to a test endpoint for demo purposes
+    const req = request.post("https://httpbin.org/post");
+    files.forEach(file => {
+      req.attach(file.name, file);
+    });
+    req.end();
+  };
+
+  dropzoneStyles = {
+    boxSizing: "border-box",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: " center",
+    justifyContent: "center",
+    width: "756px",
+    height: "220px",
+    borderRadius: "3px",
+    backgroundColor: "#ffffff",
+    border: "dashed 1px rgba(110, 141, 189, 0.2)",
+    marginTop: "18px",
+    "&:hover": {
+      cursor: "pointer"
+    }
+  };
+
+  renderUpload() {
+    return (
       <UploadAlgorithmWrapper>
+        <SubHeadline>Running Algorithm</SubHeadline>
+        <Headline>{this.state.fileName}</Headline>
+        <DescriptionText>This may take several minutes</DescriptionText>
+      </UploadAlgorithmWrapper>
+    );
+  }
+
+  renderUploader() {
+    return (
+      <ReactDropzone onDrop={this.onDrop} style={this.dropzoneStyles}>
         <UploadIcon />
         <Headline style={{ fontSize: "22px", margin: "18px 0 6px 0" }}>
-          Drag & drop your algorithm here
+          Drag and drop your algorithm here
         </Headline>
         <DescriptionText style={{ fontSize: "15px" }}>
           or you can also
@@ -42,15 +81,28 @@ const CreateAlgorithmStepTwo = props => {
           supported file types:
           <UploadTypeDescription> .bin .h5</UploadTypeDescription>
         </DescriptionText>
-      </UploadAlgorithmWrapper>
-      <Divider />
-      <NavigationFooter>
-        <CreateAlgorithmNavigationButton type="back" />
-        <CreateAlgorithmNavigationButton type="next" />
-      </NavigationFooter>
-    </Main>
-  );
-};
+      </ReactDropzone>
+    );
+  }
+
+  render() {
+    return (
+      <Main>
+        <Headline>Upload machine learning algorithm</Headline>
+        <DescriptionText>
+          We will run your algorithm using the data you pre-processed in the
+          previous step.
+        </DescriptionText>
+        {this.state.uploading ? this.renderUpload() : this.renderUploader()}
+        <Divider />
+        <NavigationFooter>
+          <CreateAlgorithmNavigationButton type="back" />
+          <CreateAlgorithmNavigationButton type="next" />
+        </NavigationFooter>
+      </Main>
+    );
+  }
+}
 
 CreateAlgorithmStepTwo.propTypes = {};
 
